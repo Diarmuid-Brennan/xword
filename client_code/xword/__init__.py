89e1171ae571f7de38bf03f2b3bac4c05733053c
c00133947@itcarlow.ie <c00133947@itcarlow.ie>
2021-01-20 19:54:14 +0000

    Edited xword

--- client_code/xword/__init__.py
+++ client_code/xword/__init__.py
@@ -1,8 +1,5 @@
 from ._anvil_designer import xwordTemplate
 from anvil import *
-with open("static/words") as wf:
-    words = {line.strip("\n").strip("'s").lower() for line in wf}  # A set.
-words = sorted(words)[1:]  # Ignore the empty word at the start of the list.
 
 class xword(xwordTemplate):
   def __init__(self, **properties):
@@ -15,22 +12,6 @@ class xword(xwordTemplate):
     """This method is called when the button is clicked"""
     pass
   
-  def find_possible_matches(pattern):
-      """Given any pattern of the type "__a___b__c", this function
-        looks up and returns all the potential matches for the
-        pattern in the Linux dictionary of words."""
-  
-      def match_pattern(w, p):
-          """Returns True if 'w' matches 'p', False otherwise."""
-          letters = {k: v for k, v in enumerate(p) if v != "_"}
-          return not any([w[i] != p[i] for i in letters.keys()])
-  
-      pattern = pattern.lower()  # Just in case...
-      matches = {
-          word  ## SELECT...
-          for word in words  ## FROM...
-          if len(word) == len(pattern) and match_pattern(word, pattern)  ## WHERE...
-      }
-      return matches
+ 
 
 
