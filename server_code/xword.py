264ae3bb8fb3d83310321a3c36d6908d0f62e36e
c00133947@itcarlow.ie <c00133947@itcarlow.ie>
2021-02-04 20:00:22 +0000

    Edited app config and xword

--- server_code/xword.py
+++ server_code/xword.py
@@ -3,14 +3,17 @@ from anvil.google.drive import app_files
 import anvil.server
 
 file = str(app_files.words.get_bytes(), 'UTF-8')
+newWordfile = str(app_files.newwords_txt.get_bytes(), 'UTF-8')
 #print(type(file))
 file = file.split()
+newWordfile = newWordfile.split()
 #words = {line.strip("\n").strip("'s").lower() for line in file}
 words = {line.strip("\n").replace("'s",'').lower() for line in file}
+newWords = {line.strip("\n").replace("'s",'').lower() for line in newWordfile}
 print(type(words)) 
 words = sorted(words)[1:]  # Ignore the empty word at the start of the list.
 #print(words)
-print(type(words))
+print(len(words))
 
 @anvil.server.callable
 def find_possible_matches(pattern):
@@ -31,21 +34,30 @@ def find_possible_matches(pattern):
         ]
     return matches
   
+
   
-@anvil.server.http_endpoint('/pattern/:pat', methods=["POST"])
-def find_matches(pat, **q):
-  pat = anvil.server.request.body_json['word']
-  result = find_possible_matches(pat)
-  return result
+def add_to_wordList(word):
+  words.append(word)
+  newWords.append(word)
+  updated ='\n'.join(words)
+  newWordsUpdated ='\n'.join(words)
+  app_files.words.set_bytes(updated)
+  app_files.newWords.set_bytes(newWordsUpdated)
+  return words
+  
+  
+#@anvil.server.http_endpoint('/pattern/:pat', methods=["POST"])
+#def find_matches(pat, **q):
+#  pat = anvil.server.request.body_json['word']
+#   result = find_possible_matches(pat)
+#  return result
+
 
 @anvil.server.http_endpoint('/pattern/add', methods=["POST"])
-def find_matches(insertword, **q):
+def find_matches(**q):
   word = anvil.server.request.body_json['word']
-  word = find_possible_matches(word)
-  if(len(word) == 0):
-    print("Not Found")
-    words.append(word)
-    s1='\n'.join(words)
-    print(s1)
-    app_files.words.set_bytes(s1)
-    return words
+  matches = find_possible_matches(word)
+  if(len(matches) == 0):
+    newlist = add_to_wordList(word)
+    return len(newlist)
+      
